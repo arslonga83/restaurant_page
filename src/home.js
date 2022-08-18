85c4ecd26cb1c434bc30afac8fb369758dea2f37
arslonga83 <jlwood83@gmail.com>
2022-08-17 20:49:24 -0700

    change ids to classes

--- src/home.js
+++ src/home.js
@@ -6,32 +6,29 @@ export function load() {
     const content = document.getElementById('content');
    
     const header = document.createElement('div');
-    header.id = 'header';
+    header.classList.add('header');
     const h1 = header.appendChild(document.createElement('h1'))
     h1.textContent = 'Jeff\'s Pizza Kitchen';
     content.appendChild(header);
 
     const nav = document.createElement('div');
-    nav.id = 'nav';
+    nav.classList.add('nav');
     const btn1 = nav.appendChild(document.createElement('button'));
-    btn1.id = 'home';
     btn1.innerHTML = 'HOME';
     const btn2 = nav.appendChild(document.createElement('button'));
-    btn2.id = 'menu';
     btn2.innerHTML = 'MENU';
     const btn3 = nav.appendChild(document.createElement('button'));
-    btn3.id = 'contact';
     btn3.innerHTML = 'CONTACT';
     content.appendChild(nav);
 
     const main = document.createElement('div');
-    main.id = 'main';
+    main.classList.add('main');
     const img = main.appendChild(document.createElement('img'));
     img.src = '../src/shaian-ramesht-exSEmuA7R7k-unsplash.jpg';
     img.alt = 'pizza';
     
     const text = main.appendChild(document.createElement('div'));
-    text.id = 'text';
+    text.classList.add('text');
     const p1 = text.appendChild(document.createElement('p'));
     const p2 = text.appendChild(document.createElement('p'));
     p1.innerHTML = 'The best vegan pies in Tigard are definitely the ones you make yourself. We\'ve been perfecting our technique since we first bought a pizza stone, and still have a good track record of not dropping dough on the floor (except for the toddler!) Farm fresh tomatoes and basil from the garden make our pizza pretty much the perfect food.';
