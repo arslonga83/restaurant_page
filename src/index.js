f0d3003bfffb2c752e20b3df1d178b71877a81b7
arslonga83 <jlwood83@gmail.com>
2022-08-17 21:38:25 -0700

    add simple button color to indicate selected tab

--- src/index.js
+++ src/index.js
@@ -11,20 +11,27 @@ loadMain();
 
 const menuBtn = document.getElementById('menu');
 menuBtn.addEventListener('click', () => {
+    menuBtn.style.color = 'red';
+    contactBtn.style.color = 'white';
+    homeBtn.style.color = 'white';
     loadMenu();
 }
 )
 
 const contactBtn = document.getElementById('contact');
 contactBtn.addEventListener('click', () => {
+    menuBtn.style.color = 'white';
+    contactBtn.style.color = 'red';
+    homeBtn.style.color = 'white';
     loadContact();
 }
 )
 
 const homeBtn = document.getElementById('home');
 homeBtn.addEventListener('click', () => {
-    const main = document.getElementById('main');
-    main.innerHTML = '';
+    menuBtn.style.color = 'white';
+    contactBtn.style.color = 'white';
+    homeBtn.style.color = 'red';
     loadMain();
 })
 
